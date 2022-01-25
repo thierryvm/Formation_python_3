7b0a5e2536aa21dd9e23b653d4fb259811a00d20
thierryvm <thierryvm@hotmail.com>
2022-01-25 18:56:20 +0100

    Exercice sur les fonction, création d'une fonction pour la table de multiplication avec gestion des erreurs

--- Exercices/Tables_de_multiplications.py
+++ Exercices/Tables_de_multiplications.py
@@ -11,3 +11,19 @@
 # Afficher_table_multiplication(n)
 # min / max
 # erreur : si min > max
+
+def table_de_multiplication(n, min=1, max=10):
+    if min > max:
+        print("ERREUR: Le min est supérieur au max")
+        return
+
+    for i in range(min, max + 1):
+        print(f'{i} x {n} = {i * n}')
+
+
+while True:
+    N = int(input("Veuillez saisir un nombre : "))
+    if N > 0:
+        break
+
+table_de_multiplication(N, 10, 1)
