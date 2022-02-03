a80df29ec9f6aa04cdc821a3ea063c3685933285
thierryvm <thierryvm@hotmail.com>
2022-02-03 09:23:53 +0100

    Nombre variable de paramètres (*)

---
+++ Testes_scripts/nombre_variable_paramètres.py
@@ -0,0 +1,15 @@
+def somme(titre, **nombres):
+    """
+
+    :param titre: on indique un titre
+    :param nombres: on récupère ici toutes les valeurs en mettant(*) ou (**) si on veut ajouter des clés
+    :return: les paramètres titre et **nombres et on ajoute dans la boucle .value() pour pouvoir utiliser les clés
+    """
+    print("TITRE:", titre)
+    resultat = 0
+    for n in nombres.values():
+        resultat += n
+    return resultat
+
+
+print(somme("La moyenne de vos notes est de :", math=5, geo=10, anglais=4))
