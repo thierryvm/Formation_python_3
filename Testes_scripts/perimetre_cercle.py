0538b89ad1c817b2ad52865c9228ffa6567f1f27
thierryvm <thierryvm@hotmail.com>
2022-02-10 16:36:05 +0100

    Script pour calculer le périmètre et l'air d'un cercle avec l'import from math import pi

---
+++ Testes_scripts/perimetre_cercle.py
@@ -0,0 +1,16 @@
+from math import pi
+
+
+def circle_perimeter(rayon):
+    return 2 * pi * rayon
+
+
+def aire_cercle(rayon):
+    return pi*rayon**2
+
+
+perimetre = circle_perimeter(1)
+print(perimetre)
+
+aire = aire_cercle(1)
+print(aire)
