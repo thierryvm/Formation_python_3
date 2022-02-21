b5413be427bfae1d6d8ddef2947451af5b285812
thierryvm <thierryvm@hotmail.com>
2022-02-21 21:27:17 +0100

    Modification du programme et amélioration du code

--- Testes_scripts/juste_prix.py
+++ Testes_scripts/juste_prix.py
@@ -5,7 +5,7 @@ from random import randint
 
 
 def main():
-    global essais
+    essais: int = 0
     NOMBRE_ESSAIS = 10
     i = 0
     NOMBRE_A_DEVINER = randint(1, 100)
@@ -31,3 +31,5 @@ def main():
 
     print('Fin du jeu.')
 
+
+main()
