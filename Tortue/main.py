c732b480c53bc36d78e73f0b4106b4a2a2a0269d
thierryvm <thierryvm@hotmail.com>
2022-02-22 11:29:50 +0100

    Optimisation du programme et amélioration du code

--- Tortue/main.py
+++ Tortue/main.py
@@ -2,7 +2,7 @@ import turtle
 
 
 def escalier(taille, nb):
-    for i in range(0, nb):
+    for _ in range(nb):
         t.forward(taille)
         t.left(90)
         t.forward(taille)
@@ -16,14 +16,14 @@ t = turtle.Turtle()
 # escalier(60, 5)
 
 def carre(taille):
-    for i in range(0, 4):
+    for _ in range(4):
         t.forward(taille)
         t.left(90)
 
 
-# On créer une nouvelle fonction qui fait appel à la précédente pour créer plusieurs carres
+# On crée une nouvelle fonction qui fait appel à la précédente pour créer plusieurs carres
 def carres(taille_de_depart, nb):
-    for i in range(0, nb):
+    for i in range(nb):
         taille = (i + 1) * taille_de_depart
         carre(taille)
 
